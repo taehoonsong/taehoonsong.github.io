1b0983d52a5facc8fd203983f48ba40d937cad88
Tae Song <email@taehoonsong.com>
2025-11-10 23:49:32 -0500

    add options to build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,34 @@
 #!/usr/bin/env bash
 
-# Render all templates
-uv run generate_website.py
+USER_OPT=$1
 
+gen_html(){
+    uv run generate_website.py
+}
 
-if [ ! -f "./pages/resume.tex" ]; then 
-    exit 0 
-fi
+gen_pdf(){
 
-# Convert resume.tex to pdf using pdflatex
-pdflatex --output-directory=pages ./pages/resume.tex
+    if [ ! -f "./pages/resume.tex" ]; then 
+        echo resume.tex missing... try running 'build.sh html'
+        exit 0
+    fi
 
-# clean up
-cd pages
-latexmk -c
+    # Convert resume.tex to pdf using pdflatex
+    pdflatex --output-directory=pages ./pages/resume.tex
+
+    # clean up
+    cd pages
+    latexmk -c
+}
+
+main(){
+
+    case $USER_OPT in
+        html) gen_html ;;
+        pdf) gen_pdf;;
+        all) gen_html && gen_pdf ;;
+        *) echo valid options are html, pdf, or all ;;
+    esac
+}
+
+main
